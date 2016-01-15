fa4893de229779961f3e7502043c1d66b37253f2
jword <738594929@qq.com>
2016-01-15 09:09:59 +0800

    修改命令行程序

--- app/Console/Commands/vote.php
+++ app/Console/Commands/vote.php
@@ -1,6 +1,5 @@
 <?php namespace App\Console\Commands;
 
-use Curl\Curl;
 use Illuminate\Console\Command;
 
 class Vote extends Command
@@ -9,13 +8,13 @@ class Vote extends Command
      * 命令名称.
      * @var string
      */
-    protected $name = 'task:vote';
+    protected $name = 'task:test';
 
     /**
      * 命令简介.
      * @var string
      */
-    protected $description = '投票刷票程序';
+    protected $description = 'test task.';
 
     /**
      * 执行命令.
@@ -23,79 +22,9 @@ class Vote extends Command
      */
     public function fire()
     {
-        $useragent = array(
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36 QQBrowser/3.9.3943.400',
-            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
-            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
-            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
-            'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
-            'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
-            'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
-            'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
-            'Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13',
-            'MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
-            'Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
-            'Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
-            'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19',
-            'Mozilla/5.0 (Linux; U; Android 2.3.6; zh-cn; GT-S5660 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255',
-            'Mozilla/5.0 (Linux; U; Android 2.2.1; zh-cn; HTC_Wildfire_A3333 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
-            'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)',
-            'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Ubuntu/11.10 Chromium/27.0.1453.93 Chrome/27.0.1453.93 Safari/537.36',
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Ubuntu/11.10 Chromium/27.0.1453.93 Chrome/27.0.1453.93 Safari/537.36',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:34.0) Gecko/20100101 Firefox/34.0',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36 QQBrowser/3.9.3943.400',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:43.0) Gecko/20100101 Firefox/43.0',
-        );
-        $x = 0;
-        while (true) {
-            $agent = array_rand($useragent);
-            $curl  = new Curl();
-            $curl->setUserAgent($agent);
-            //获取伪造的ip
-            $ip  = file_get_contents('http://192.168.8.18:12345/?opt=get&num=1');
-            $ips = explode(':', $ip);
-            if (isset($ips[0]) && $ips[0]) {
-                $curl->setHeader('CLIENT-IP', $ips[0]);
-                $curl->setHeader('X-FORWARDED-FOR', $ips[0]);
-            }
 
-            $curl->setHeader('Origin', 'http://henan.qq.com');
-            $curl->setHeader('Host', 'panshi.qq.com');
-            $curl->setHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
-            $curl->setHeader('Content-Type', 'application/x-www-form-urlencoded');
-            $re = $curl->post(
-                'http://panshi.qq.com/v2/vote/10513076/submit',
-                array(
-                    'answer'   => '{"561490":{"selected":[1303761]}}',
-                    'source'   => '1',
-                    'login'    => '1',
-                    'g_tk'     => '2013',
-                    'format'   => 'script',
-                    'callback' => 'parent.AppPlatform.Survey.Digg.ReceiveDiggResult',
-                )
-            );
-            $result = substr($re, 109, -21);
-            $result = json_decode($result, true);
-            if (empty($result)) {
-                $status = $re . PHP_EOL;
-            } else {
-                $result = $result['data']['subject'][0];
-                $status = $result['total'] . ':' . $result['option'][11]['selected'] . ':' . $result['option'][11]['percent'] . PHP_EOL;
-            }
-            echo $status;
-            $x++;
-            sleep(1);
+        while (true) {
+            echo '1234' . PHP_EOL;
         }
     }
 }
